703c01579a39afb8860a1bdd9cbe3eec0340a64d
Albin Bramstång <carlalbing@hotmail.com>
2011-09-27 10:43:57 +0200

    Started implementing the Google Maps.

---
+++ src/nu/placebo/whatsup/MapViewActivity.java
@@ -0,0 +1,21 @@
+package nu.placebo.whatsup;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.maps.*;
+
+public class MapViewActivity extends Activity {
+	
+	//private MapView mapView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+	
+	
+	
+}
