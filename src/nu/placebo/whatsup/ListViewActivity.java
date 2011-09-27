ddf25f5c4d938bb723fbef3a7ade69a92e567581
kirayatail <kirayatail@gmail.com>
2011-09-27 12:36:00 +0200

    Created ListViewActivity files

---
+++ src/nu/placebo/whatsup/ListViewActivity.java
@@ -0,0 +1,14 @@
+package nu.placebo.whatsup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListViewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+}
