3792bd2316795993a0a58f52a4ab966ff4548856
kirayatail <kirayatail@gmail.com>
2011-09-27 13:06:47 +0200

    More work done on ListViewActivity

--- src/nu/placebo/whatsup/ListViewActivity.java
+++ src/nu/placebo/whatsup/ListViewActivity.java
@@ -2,13 +2,20 @@ package nu.placebo.whatsup;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/**
+ * Activity showing annotations in a list view.
+ * 
+ * @author max
+ *
+ */
 public class ListViewActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.listview);
 	}
+	
+	
 	
 }
