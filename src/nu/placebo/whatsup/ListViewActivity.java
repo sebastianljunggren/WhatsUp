363680299c9ce0a9bac23722bc9ae2039f61c89e
kirayatail <kirayatail@gmail.com>
2011-09-28 12:41:03 +0200

    Work on ListAdapter still to be done... Intermediate commit

--- src/nu/placebo/whatsup/ListViewActivity.java
+++ src/nu/placebo/whatsup/ListViewActivity.java
@@ -1,7 +1,9 @@
 package nu.placebo.whatsup;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import android.view.*;
 import android.widget.ArrayAdapter;
 import nu.placebo.whatsup.model.ListMarker;
 import android.app.Activity;
@@ -22,13 +24,35 @@ public class ListViewActivity extends Activity {
 	}
 	
 	
+	
+	
+	
+	
 	private class MarkerAdapter extends ArrayAdapter<ListMarker> {
-
+		
+		private ArrayList<ListMarker> markers;
 		public MarkerAdapter(Context context, int textViewResourceId,
 				List<ListMarker> objects) {
 			super(context, textViewResourceId, objects);
-			// TODO Auto-generated constructor stub
+			
 		}
+		
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if(v == null){
+				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = vi.inflate(R.layout.list_item, null);
+			}
+			ListMarker lm = markers.get(position);
+			if(lm != null){
+				// Find text views and fill them with info from ListMarker
+			}
+			
+			return v;
+		}
+		
 		
 	}
 }
