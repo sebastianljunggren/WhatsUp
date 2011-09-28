7ade16d5769a055826d00b3e5131b43bd86b26e7
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2011-09-28 19:09:31 +0200

    Fixed annotation class.

--- src/nu/placebo/whatsup/model/Annotation.java
+++ src/nu/placebo/whatsup/model/Annotation.java
@@ -5,18 +5,38 @@ import org.json.JSONObject;
 
 public class Annotation {
 	private GeoLocation geolocation;
+	private String body;
+	private int uid;
+	private String author;
 
 	public Annotation(String json) throws JSONException {
 		this(new JSONObject(json));
 	}
 
 	public Annotation(JSONObject json) throws JSONException {
+		JSONObject location = json.getJSONObject("location");
+		this.body = json.getJSONObject("body").getJSONObject("und")
+				.getString("safe_value");
 		this.geolocation = new GeoLocation(json.getInt("nid"),
-				json.getDouble("hej"), json.getDouble("hej"), "hej");
+				location.getDouble("latitude"),
+				location.getDouble("longitude"), json.getString("title"));
+		this.uid = json.getInt("uid");
+		this.author = json.getString("name");
 	}
-	// TODO Define what an annotation consists of more than a GeoLocation.
 
 	public GeoLocation getGeolocation() {
 		return geolocation;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public String getAuthor() {
+		return author;
 	}
 }
