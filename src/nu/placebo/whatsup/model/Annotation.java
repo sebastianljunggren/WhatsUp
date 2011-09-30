689f33daf140421e52b11a0748626e0a44f6a061
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2011-09-30 14:11:12 +0200

    Added AnnotationActivity and made it possible to open it from the main activity.

--- src/nu/placebo/whatsup/model/Annotation.java
+++ src/nu/placebo/whatsup/model/Annotation.java
@@ -4,10 +4,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Annotation {
-	private GeoLocation geolocation;
+	private GeoLocation geoLocation;
 	private String body;
 	private int uid;
 	private String author;
+
+	public Annotation(GeoLocation geoLocation, String body, int uid,
+			String author) {
+		this.geoLocation = new GeoLocation(geoLocation);
+		this.body = body;
+		this.author = author;
+		this.uid = uid;
+	}
 
 	public Annotation(String json) throws JSONException {
 		this(new JSONObject(json));
@@ -15,17 +23,17 @@ public class Annotation {
 
 	public Annotation(JSONObject json) throws JSONException {
 		JSONObject location = json.getJSONObject("location");
-		this.body = json.getJSONObject("body").getJSONArray("und").getJSONObject(0)
-				.getString("safe_value");
-		this.geolocation = new GeoLocation(json.getInt("nid"),
+		this.body = json.getJSONObject("body").getJSONArray("und")
+				.getJSONObject(0).getString("safe_value");
+		this.geoLocation = new GeoLocation(json.getInt("nid"),
 				location.getDouble("latitude"),
 				location.getDouble("longitude"), json.getString("title"));
 		this.uid = json.getInt("uid");
 		this.author = json.getString("name");
 	}
 
-	public GeoLocation getGeolocation() {
-		return geolocation;
+	public GeoLocation getGeoLocation() {
+		return geoLocation;
 	}
 
 	public String getBody() {
