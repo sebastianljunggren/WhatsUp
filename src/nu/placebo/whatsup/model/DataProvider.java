61a90d8f3961ca80004d6c4eb4640b7447f397a6
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2011-10-03 23:27:55 +0200

    Added retrieve method stubs to the DataProvider and made the distinction between retrieving from the server and getting from the local database.

--- src/nu/placebo/whatsup/model/DataProvider.java
+++ src/nu/placebo/whatsup/model/DataProvider.java
@@ -15,31 +15,46 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-	
+
 public class DataProvider {
-	
+
 	/**
 	 * Class is Singelton, disallow creation
 	 */
-	private DataProvider() {}
-	
+	private DataProvider() {
+	}
+
 	private static final DataProvider instance = new DataProvider();
-	
+
 	private DatabaseHelper dbHelper = new DatabaseHelper(null);
-	
+
 	public static DataProvider getDataProvider() {
 		return instance;
 	}
-	
+
 	public Annotation getAnnotation(int nid) {
-		//Calling local cache first, then putting a network call in the queue, returning an object containing the content of the local
-		//cache, and means of acquiring the data fetched from remote server
+		// Fetches an annotation from the local database.
 		return null;
 	}
-	
+
 	public List<GeoLocation> getAnnotationMarkers(double latitudeA,
 			double longitudeA, double latitudeB, double longitudeB) {
-		//Same idea as above
+		// Fetches GeoLocations from the local database.
 		return null;
+	}
+
+	public void retrieveAnnotation(int nid) {
+		// Fetches an annotation from the server and stores it in the local
+		// database.
+		// TODO Auto-generated method stub
+
+	}
+
+	public void retrieveGeoLocations(double latitudeA, double longitudeA,
+			double latitudeB, double longitudeB) {
+		// Fetches GeoLocations from the server and stores them in the local
+		// database.
+		// TODO Auto-generated method stub
+
 	}
 }
