7e8b1afe099aff82459e4339e35a18371c37302e
kirayatail <kirayatail@gmail.com>
2011-10-04 15:12:45 +0200

    Documentation on pythagoras method

--- src/nu/placebo/whatsup/util/Geodetics.java
+++ src/nu/placebo/whatsup/util/Geodetics.java
@@ -6,7 +6,7 @@ public class Geodetics {
 	
 	/**
 	 * Haversine formula for calculating distance between two points denoted with microdegree lat/long coordinates.
-	 * 
+	 * This method takes the shortest way over a great circle, which makes it more accurate on longer distances on any bearing.
 	 * 
 	 * 
 	 * @param a - GeoPoint a
@@ -26,6 +26,13 @@ public class Geodetics {
 		return 2 * Math.atan2(Math.sqrt(alpha), Math.sqrt(1 - alpha)) * 6371;
 	}
 	
+	/**
+	 * Pythagoras theorem with a transverse mercator projection. Efficient and accurate enough on short distances.
+	 * 
+	 * @param a - GeoPoint
+	 * @param b - GeoPoint
+	 * @return distance in meters
+	 */
 	public static double distanceCoarse(GeoPoint a, GeoPoint b){
 		double aLat = Math.toRadians((double)a.getLatitudeE6()/1000000);
 		double bLat = Math.toRadians((double)b.getLatitudeE6()/1000000);
@@ -40,8 +47,14 @@ public class Geodetics {
 	
 	public static String distanceWithUnit(double dist){
 		String result = "";
+		if(dist<0)
+			dist = -dist;
 		
 		
+		if(dist < 1000){
+			dist = Math.round(dist);
+			result = dist+" m";
+		}
 		
 		
 		return result;
