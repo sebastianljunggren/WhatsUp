5058ebb372709b73de761e8b1eca23eeb97d0231
kirayatail <kirayatail@gmail.com>
2011-10-07 12:32:01 +0200

    PositionPicker and CreateAnnotation should link with request- and result codes. Work to be done.

--- src/nu/placebo/whatsup/activity/CreateAnnotationActivity.java
+++ src/nu/placebo/whatsup/activity/CreateAnnotationActivity.java
@@ -3,6 +3,7 @@ package nu.placebo.whatsup.activity;
 import com.google.android.maps.GeoPoint;
 
 import nu.placebo.whatsup.R;
+import nu.placebo.whatsup.model.Annotation;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,47 +11,57 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class CreateAnnotationActivity extends Activity implements OnClickListener{
+public class CreateAnnotationActivity extends Activity implements
+		OnClickListener {
 
 	private GeoPoint gp;
 	private TextView titleField;
 	private TextView descField;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		Bundle gPBundle = this.getIntent().getExtras();
-		
+
 		// if bundle does not contain a GeoPoint
 		// kill self.
-		// else 
+		// else
 		// gp = gPBundle.getGeoPoint();
-		
-		/* if SessionHandler has no session
-		 * kill self
-		 * 
+
+		/*
+		 * if SessionHandler has no session kill self
 		 */
-		
+
 		this.setContentView(R.layout.create_annotation);
 		this.titleField = (TextView) this.findViewById(R.id.create_annot_title);
 		this.descField = (TextView) this.findViewById(R.id.create_annot_desc);
-		
+
 		Button submitBtn = (Button) this.findViewById(R.id.create_annot_submit);
 		submitBtn.setOnClickListener(this);
-		
+
+	}
+
+	public void onClick(View v) {
+
+		String title = (String) titleField.getText();
+		String desc = (String) titleField.getText();
+
+		if (title != null && title != "") {
+			// Annotation a = new Annotation();
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
 	}
 	
-	public void onClick(View v){
-		
-		/*
-		 * Retrieve all data from textfields and such,
-		 * parse and validate;
-		 * create and send Annotation object to service/network caller.
-		 */
-		
-	}
 	
 
 }
--- src/nu/placebo/whatsup/activity/PositionPickerActivity.java
+++ src/nu/placebo/whatsup/activity/PositionPickerActivity.java
@@ -1,9 +1,9 @@
 package nu.placebo.whatsup.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 
 /**
  * Clean map for picking positions
@@ -11,28 +11,44 @@ import android.view.View;
  * 
  * 
  * @author max
- *
+ * 
  */
 public class PositionPickerActivity extends Activity {
-	
+
+	private boolean isCreateAnnotation;
+
 	@Override
-	public void onCreate(Bundle savedInstance){
+	public void onCreate(Bundle savedInstance) {
 		/*
-		 * Validate the incoming bundle, and find out whether the next activity should be CreateAnnotationActivity
-		 * or CreateReferenceActivity
+		 * Validate the incoming bundle, and find out whether the next activity
+		 * should be CreateAnnotationActivity or CreateReferenceActivity
 		 * 
 		 * If bundle is empty or doesn't match either, kill self.
 		 * 
-		 * if next activity is CreateAnnotation, check with SessionHandler if logged in. If not, start log in activity.
+		 * if next activity is CreateAnnotation, check with SessionHandler if
+		 * logged in. If not, start log in activity.
 		 */
 	}
-	
-	public void onClick(View v){
-		/*
-		 * On "done" (or equal)
-		 * 
-		 * Start CreateAnnotation or CreateReference-activity and provide the GeoPosition picked in the intent.
-		 * 
-		 */
+
+	public void onClick(View v) {
+		if (this.isCreateAnnotation) {
+			Intent intent = new Intent(this, CreateAnnotationActivity.class);
+			this.startActivityForResult(intent, 1);
+		}
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onActivityResult(int, int,
+	 * android.content.Intent)
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		// Activity should kill self if result code means that the child
+		// submitted its data and everything is OK.
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
 }
