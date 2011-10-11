baa935821d60a6a7260e482583d7bb857a9743e8
kirayatail <kirayatail@gmail.com>
2011-10-11 17:32:31 +0200

    Linking to dataprovider

--- src/nu/placebo/whatsup/activity/RefPointActivity.java
+++ src/nu/placebo/whatsup/activity/RefPointActivity.java
@@ -1,11 +1,13 @@
 package nu.placebo.whatsup.activity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import nu.placebo.whatsup.R;
 import nu.placebo.whatsup.constants.Constants;
-import nu.placebo.whatsup.model.GeoLocation;
-import nu.placebo.whatsup.model.ListMarker;
+import nu.placebo.whatsup.model.ReferencePoint;
+import nu.placebo.whatsup.service.model.DataProvider;
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
@@ -20,57 +22,47 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class RefPointActivity extends ListActivity {
-	
-	private ArrayList<GeoLocation> refs;
+
+	private List<ReferencePoint> refs;
 	private MarkerAdapter adapter;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.ref_point_activity);
 		Log.d("whatsup", "Starting refPointActivity");
-		
-		
-		
-		refs = new ArrayList<GeoLocation>();
-		refs.add(new GeoLocation(1, 57708759, 11937507, "Lindholmen"));
-		refs.add(new GeoLocation(2, 57687959, 11978865, "Linsen"));
-		
-		this.adapter = new MarkerAdapter(this, R.layout.ref_item, new ArrayList<GeoLocation>());
+
+		refs = new ArrayList<ReferencePoint>();
+
+		this.adapter = new MarkerAdapter(this, R.layout.ref_item,
+				new ArrayList<ReferencePoint>());
 		this.setListAdapter(adapter);
+		
+		refresh();
+
+		
+
+	}
+	
+	private void refresh(){
+		refs = DataProvider.getDataProvider(this).getAllReferencePoints();
 		
 		adapter.markers.clear();
 		adapter.notifyDataSetChanged();
-		for (int i = 0; i < refs.size(); i++){
+		for (int i = 0; i < refs.size(); i++) {
 			adapter.add(refs.get(i));
-			
+
 		}
 		adapter.notifyDataSetChanged();
-		
-	//	this.runOnUiThread(returnRes);
-		
-		
 	}
-	
-	private Runnable returnRes = new Runnable() {
 
-		public void run() {
-			if (refs != null && refs.size() > 0) {
-				
-			}
-			// m_ProgressDialog.dismiss();
-			
-		}
-	};
-	
-	
-	private class MarkerAdapter extends ArrayAdapter<GeoLocation> implements
+	private class MarkerAdapter extends ArrayAdapter<ReferencePoint> implements
 			OnClickListener {
-		private Context ctx;
-		private ArrayList<GeoLocation> markers;
+		private Activity ctx;
+		private ArrayList<ReferencePoint> markers;
 
-		public MarkerAdapter(Context context, int textViewResourceId,
-				ArrayList<GeoLocation> objects) {
+		public MarkerAdapter(Activity context, int textViewResourceId,
+				ArrayList<ReferencePoint> objects) {
 			super(context, textViewResourceId, objects);
 			this.markers = objects;
 			this.ctx = context;
@@ -83,16 +75,17 @@ public class RefPointActivity extends ListActivity {
 				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 				v = vi.inflate(R.layout.ref_item, null);
 			}
-			GeoLocation gl = markers.get(position);
+			ReferencePoint gl = markers.get(position);
 			if (gl != null) {
 				TextView t_title = (TextView) v
 						.findViewById(R.id.ref_item_title);
 
 				if (t_title != null)
-					t_title.setText(gl.getTitle());
+					t_title.setText(gl.getName());
 				v.setId(R.id.ref_id);
 				v.setTag(R.id.ref_id, gl.getId());
-				ImageButton delBtn = (ImageButton) v.findViewById(R.id.ref_item_delete);
+				ImageButton delBtn = (ImageButton) v
+						.findViewById(R.id.ref_item_delete);
 				delBtn.setTag(R.id.ref_id, gl.getId());
 				delBtn.setOnClickListener(this);
 				v.setOnClickListener(this);
@@ -105,13 +98,17 @@ public class RefPointActivity extends ListActivity {
 			if (v.getId() == R.id.ref_item_delete) {
 				Log.d("whatsup",
 						"Delete reference point: "
-								+ ((Integer) v
-										.getTag(R.id.ref_id)));
+								+ ((Integer) v.getTag(R.id.ref_id)));
+				DataProvider.getDataProvider(ctx).removeReferencePoint(
+						(Integer) v.getTag(R.id.ref_id));
+				((RefPointActivity) ctx).refresh();
+
 			} else if (v.getId() == R.id.ref_id) {
 				Log.d("whatsup",
 						"Select reference point: "
-								+ ((Integer) v
-										.getTag(R.id.ref_id)));
+								+ ((Integer) v.getTag(R.id.ref_id)));
+				Intent intent = new Intent(ctx, PositionPickerActivity.class);
+				ctx.startActivityForResult(intent, Constants.REFERENCE_POINT);
 			}
 
 		}
