93a10a69627257f66fabc59450a8b9e3d2e57f62
kirayatail <kirayatail@gmail.com>
2011-10-11 17:05:07 +0200

    More tests to the distance method in Geodetics

--- WhatsUpTest/src/nu/placebo/whatsuptest/utiltest/GeodeticsTest.java
+++ WhatsUpTest/src/nu/placebo/whatsuptest/utiltest/GeodeticsTest.java
@@ -1,7 +1,7 @@
 package nu.placebo.whatsuptest.utiltest;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import com.google.android.maps.GeoPoint;
+
 import nu.placebo.whatsup.util.Geodetics;
 import android.test.AndroidTestCase;
 
@@ -18,7 +18,7 @@ public class GeodeticsTest extends AndroidTestCase {
 	
 	
 	
-	public void testDistanceMeterPrecision(){
+	public void testPresentMeterPrecision(){
 		double hundredmeters = 100.4;
 		
 		String result = Geodetics.distanceWithUnit(hundredmeters);
@@ -28,7 +28,7 @@ public class GeodeticsTest extends AndroidTestCase {
 		
 	}
 	
-	public void testDistanceKMeterPrecision(){
+	public void testPresentKMeterPrecision(){
 		double nearlyKm = 999.7;
 		
 		String result = Geodetics.distanceWithUnit(nearlyKm);
@@ -37,7 +37,7 @@ public class GeodeticsTest extends AndroidTestCase {
 	}
 	
 	
-	public void testDistanceFarLimit(){
+	public void testPresentFarLimit(){
 		/*
 		 * The far limit is 1000 km exclusive, which may present 999.9 km. 
 		 * 1000.0 km should thus never be displayed.
@@ -48,5 +48,38 @@ public class GeodeticsTest extends AndroidTestCase {
 		String result = Geodetics.distanceWithUnit(nearlyFar);
 		
 		assertEquals("999.970 m returns far away", "Very far away", result);
+	}
+	
+	public void testDistanceOne(){
+		GeoPoint a = new GeoPoint(55000000, 12000000);
+		GeoPoint b = new GeoPoint(55000009, 12000000);
+		
+		double distance = Math.round(Geodetics.distance(a, b));
+		
+		assertEquals("Distance should be 1 meter", 1.0, distance);
+	}
+	
+	public void testDistanceEquator(){
+		/*
+		 * Testing one degree at equator
+		 */
+		
+		GeoPoint a = new GeoPoint(0, 12000000);
+		GeoPoint b = new GeoPoint(0, 13000000);
+		
+		double distance = Math.round(Geodetics.distance(a, b)/100);
+		/*
+		 * Tolerate within 100 m
+		 */
+		
+		
+		assertEquals("Equator: Distance should be 1112 hm", 1112.0, distance);
+		
+	}
+	
+	public void testDistanceFarNorth(){
+		/*
+		 * Testing one degree very far north
+		 */
 	}
 }
