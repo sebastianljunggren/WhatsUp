d4afd71d83b934d621642e166dce05864f59fdb2
Sebastian Ljunggren <sebastianljunggren@gmail.com>
2011-10-13 17:59:23 +0200

    Fixes to tests.

--- WhatsUpTest/src/nu/placebo/whatsuptest/networktest/NetworkTest.java
+++ WhatsUpTest/src/nu/placebo/whatsuptest/networktest/NetworkTest.java
@@ -8,64 +8,117 @@ import nu.placebo.whatsup.network.NetworkOperationListener;
 import nu.placebo.whatsup.network.OperationResult;
 import nu.placebo.whatsup.network.SessionTest;
 import android.test.AndroidTestCase;
-
+import android.util.Log;
+/**
+ * 
+ * Test the network calls
+ *
+ */
 public class NetworkTest extends AndroidTestCase {
 
-	private SessionInfo session;
-
-	public NetworkTest(){
+	public NetworkTest() {
 		super();
 	}
-	
-	public void setUp() throws Exception{
-		
+
+	public void setUp() throws Exception {
+
 		super.setUp();
 	}
-	
-	public void testAnnotationRetrieve(){
-		AnnotationRetrieve a = new AnnotationRetrieve(1234);
+
+	public void testAnnotationRetrieveFail() {
+		AnnotationRetrieve a = new AnnotationRetrieve(0);
 		assertTrue(true);
 		a.addOperationListener(new NetworkOperationListener<Annotation>() {
 
 			@Override
 			public void operationExcecuted(OperationResult<Annotation> result) {
-				assertFalse("Errors when retrieving annotation", result.hasErrors());
-				assertNotNull("Annotation reurned was null", result.getResult());
-				assertEquals("Incorrect nid on the Annotation returned.", 1234, result.getResult().getId());
+				assertTrue("Errors when retrieving annotation",
+						result.hasErrors());
+				assertNull("Annotation returned was not null",
+						result.getResult());
+				assertTrue("Error code not set, ",
+						result.getStatusCode() >= 300);
 			}
-			
+
 		});
 		OperationResult<Annotation> result = a.execute();
 		a.notifyListeners(result);
 	}
-	
-	public void testLogIn() {
+
+	public void testAnnotationRetrieve() {
+		AnnotationRetrieve a = new AnnotationRetrieve(1337);
+		assertTrue(true);
+		a.addOperationListener(new NetworkOperationListener<Annotation>() {
+
+			@Override
+			public void operationExcecuted(OperationResult<Annotation> result) {
+				assertFalse("Errors when retrieving annotation",
+						result.hasErrors());
+				assertNotNull("Annotation returned was null", result.getResult());
+				assertEquals("Incorrect nid on the Annotation returned.", 1337,
+						result.getResult().getId());
+			}
+
+		});
+		OperationResult<Annotation> result = a.execute();
+		a.notifyListeners(result);
+	}
+
+	public void testSession() {
 		Login l = new Login("test", "WhatsUp!");
 		l.addOperationListener(new NetworkOperationListener<SessionInfo>() {
 
 			@Override
 			public void operationExcecuted(OperationResult<SessionInfo> result) {
 				assertFalse("Errors when loggin in", result.hasErrors());
-				assertNotNull("SessionInfo returned was null",
-						 session = result.getResult());
+				assertNotNull("SessionInfo returned was null",result.getResult());
 			}
 
 		});
 		OperationResult<SessionInfo> result = l.execute();
-		this.session  = result.getResult();
-		this.session.getSessionId();
+		final SessionInfo session = result.getResult();
 		l.notifyListeners(result);
-	}
-	
-	public void testSessionTest(){
+		
 		SessionTest st = new SessionTest(session);
+		Log.w("WhatsUp", session.getSessionName());
 		st.addOperationListener(new NetworkOperationListener<SessionInfo>() {
 
 			@Override
 			public void operationExcecuted(OperationResult<SessionInfo> result) {
-				assertFalse("Errors when testing session from log in.", result.hasErrors());
-				assertNotNull("SessionInfo returned was null",
-						 session = result.getResult());
+				assertFalse("Errors when testing session from log in.",
+						result.hasErrors());
+				assertNotNull("SessionInfo returned was null", result.getResult());
+			}
+
+		});
+		st.setOperationResult(st.execute());
+		st.notifyListeners(st.getResult());
+	}
+	
+	public void testSessionFail() {
+		Login l = new Login("test", "WrongPass!");
+		l.addOperationListener(new NetworkOperationListener<SessionInfo>() {
+
+			@Override
+			public void operationExcecuted(OperationResult<SessionInfo> result) {
+				assertTrue("Errors not set when logging in with wrong credentials.", result.hasErrors());
+				assertNull("SessionInfo returned was not null, when logging in.",result.getResult());
+			}
+
+		});
+		OperationResult<SessionInfo> result = l.execute();
+		final SessionInfo session = result.getResult();
+		l.notifyListeners(result);
+		
+		SessionTest st = new SessionTest(session);
+		Log.w("WhatsUp", session.getSessionName());
+		st.addOperationListener(new NetworkOperationListener<SessionInfo>() {
+
+			@Override
+			public void operationExcecuted(OperationResult<SessionInfo> result) {
+				assertTrue("Errors not set when testing faulty SessionInfo.",
+						result.hasErrors());
+				assertNull("SessionInfo returned was not null when testing faulty session.", result.getResult());
 			}
 
 		});
