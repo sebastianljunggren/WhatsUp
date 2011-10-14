3f625900c8a11dbe1e7e32c47446415545a77d7b
kirayatail <kirayatail@gmail.com>
2011-10-14 10:38:21 +0200

    fix to RegisterActivity: credentials sent to login is now the same as in the server register call

--- src/nu/placebo/whatsup/activity/RegisterActivity.java
+++ src/nu/placebo/whatsup/activity/RegisterActivity.java
@@ -1,20 +1,24 @@
 package nu.placebo.whatsup.activity;
 
 import nu.placebo.whatsup.R;
+import nu.placebo.whatsup.constants.Constants;
 import nu.placebo.whatsup.model.SessionHandler;
+import nu.placebo.whatsup.model.SessionInfo;
+import nu.placebo.whatsup.network.Login;
 import nu.placebo.whatsup.network.NetworkOperationListener;
 import nu.placebo.whatsup.network.NetworkTask;
 import nu.placebo.whatsup.network.OperationResult;
 import nu.placebo.whatsup.network.RegisterOperation;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RegisterActivity extends Activity implements OnClickListener, NetworkOperationListener<Void>{
+public class RegisterActivity extends Activity implements OnClickListener, NetworkOperationListener<Void> {
 	
 	private String username;
 	private String password;
@@ -34,9 +38,9 @@ public class RegisterActivity extends Activity implements OnClickListener, Netwo
 
 	public void onClick(View v) {
 		if(v.getId() == R.id.submit){
-			String username = ((TextView) this.findViewById(R.id.user_name)).getText().toString();
-			String password = ((TextView) this.findViewById(R.id.password)).getText().toString();
-			String email = ((TextView) this.findViewById(R.id.email)).getText().toString();
+			this.username = ((TextView) this.findViewById(R.id.user_name)).getText().toString();
+			this.password = ((TextView) this.findViewById(R.id.password)).getText().toString();
+			this.email = ((TextView) this.findViewById(R.id.email)).getText().toString();
 			
 			if(username != "" && password != "" && email !=""){
 				RegisterOperation ro = new RegisterOperation(username, password, email);
@@ -50,12 +54,13 @@ public class RegisterActivity extends Activity implements OnClickListener, Netwo
 	public void operationExcecuted(OperationResult<Void> result) {
 		if(!result.hasErrors()){
 			Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show();
-			
+			Log.d("whatsup", "Registration result: "+result.getStatusCode());
 			SessionHandler.getInstance(this).saveCredentials(username, password);
 			SessionHandler.getInstance(this).login();
-			
-			this.finish();
 		}
+	}
+	
+	public void operationExecuted(OperationResult<SessionInfo> result){
 		
 	}
 }
